b62977d6c1e7aff676f75f91e2a26da28ac849ae
Clooooud <yohanbaret@hotmail.fr>
2024-01-02 04:54:23 +0100

    Rewrote the spaghetti string manipulation of the list command

--- src/main/java/com/stackmc/subserver/commands/subs/ListSubCommand.java
+++ src/main/java/com/stackmc/subserver/commands/subs/ListSubCommand.java
@@ -3,13 +3,14 @@ package com.stackmc.subserver.commands.subs;
 import com.stackmc.subserver.SubServer;
 import com.stackmc.subserver.instance.Instance;
 import lombok.RequiredArgsConstructor;
-import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class ListSubCommand implements TabExecutor {
@@ -18,26 +19,16 @@ public class ListSubCommand implements TabExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command rootCommand, String label, String[] args) {
-        final String[] message = {"Voici la liste des instances :\n"};
-        Instance.getInstances().forEach(instance -> {
-            final String[] worlds = {""};
-            instance.worlds.forEach(world -> {
-                if(instance.worlds.size() > 1) {
-                    worlds[0] = worlds[0] + world.getName() + ", ";
-                } else {
-                    worlds[0] = worlds[0] + world.getName();
-                }
-            });
-
-            message[0] = message[0] + instance.name + " (" + worlds[0] + ")\n";
-        });
-        sender.sendMessage(message[0]);
+        String message = Instance.getInstances().stream().map(instance ->  {
+            return instance.getName() + " (" + instance.getWorlds().stream().map(World::getName).collect(Collectors.joining(", ")) + ")";
+        }).collect(Collectors.joining("\n"));
+        sender.sendMessage("Voici la liste des instances :\n" + message);
         return false;
     }
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command rootCommand, String label, String[] args) {
-        return Collections.emptyList(); // ça doit être possible de trouver les noms de maps déjà existants en checkant le dossier
+        return Collections.emptyList();
     }
 
 }
